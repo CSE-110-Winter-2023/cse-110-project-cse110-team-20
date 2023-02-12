55d05502ecb469a53de8630bcf83a6589e78e998
Derrick Yao <dyao@ucsd.edu>
2023-02-12 12:51:31 -0800

    created IOrientation interface

---
+++ app/src/main/java/com/example/socialcompass/IOrientation.java
@@ -0,0 +1,9 @@
+package com.example.socialcompass;
+
+public interface IOrientation {
+    // return the orientation of the device relative to true north in radians
+    // where 0 is north, pi/2 is east, -pi/2 is west, etc.
+    // ideally, this would return a value in the range [-pi, pi]
+    // which is the range of android's sensormanager.getorientation()
+    float getOrientation();
+}
