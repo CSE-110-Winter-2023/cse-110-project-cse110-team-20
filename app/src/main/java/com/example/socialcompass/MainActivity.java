608b34cfc696d711816b961120a959ce975316e0
yangwestyyy21 <33038975+yangwestyyy21@users.noreply.github.com>
2023-02-13 00:51:01 -0800

    this task is finished

--- app/src/main/java/com/example/socialcompass/MainActivity.java
+++ app/src/main/java/com/example/socialcompass/MainActivity.java
@@ -2,7 +2,9 @@ package com.example.socialcompass;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,4 +13,10 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+    public void OnDisplayCompassClicked(View view) {
+        Intent intent = new Intent(this, Mapscreen.class);
+        startActivity(intent);
+    }
+
 }
--- app/src/main/java/com/example/socialcompass/Mapscreen.java
+++ app/src/main/java/com/example/socialcompass/Mapscreen.java
@@ -11,4 +11,5 @@ public class Mapscreen extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_mapscreen);
     }
+
 }
