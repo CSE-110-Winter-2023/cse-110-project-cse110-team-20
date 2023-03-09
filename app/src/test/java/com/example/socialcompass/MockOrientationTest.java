2f5d554d316c8a6ff4f85e25f35cd1b6d98ba8f9
Junzhe Luo <j5luo@ucsd.edu>
2023-03-08 20:47:38 -0800

    added more testcases for MockOrientationTest

--- app/src/test/java/com/example/socialcompass/MockOrientationTest.java
+++ app/src/test/java/com/example/socialcompass/MockOrientationTest.java
@@ -29,4 +29,43 @@ public class MockOrientationTest {
         mo.setOrientation(orientation2);
         assertEquals(orientation2, mo.getOrientation().getValue(), delta);
     }
+    @Test
+    public void testNegativeOrientation() {
+        float orientation = -1.0f;
+        Orientation mo = new Orientation(orientation);
+        assertEquals(orientation, mo.getOrientation().getValue(), delta);
+    }
+
+    @Test
+    public void testLargeOrientation() {
+        float orientation = 100.0f;
+        Orientation mo = new Orientation(orientation);
+        assertEquals(orientation, mo.getOrientation().getValue(), delta);
+    }
+
+    @Test
+    public void testSetNegativeOrientation() {
+        float orientation = -1.0f;
+        Orientation mo = new Orientation();
+        mo.setOrientation(orientation);
+        assertEquals(orientation, mo.getOrientation().getValue(), delta);
+    }
+
+    @Test
+    public void testSetLargeOrientation() {
+        float orientation = 100.0f;
+        Orientation mo = new Orientation();
+        mo.setOrientation(orientation);
+        assertEquals(orientation, mo.getOrientation().getValue(), delta);
+    }
+
+    @Test
+    public void testSetMultipleOrientation() {
+        float orientation1 = 1.0f;
+        float orientation2 = 2.0f;
+        Orientation mo = new Orientation(orientation1);
+        mo.setOrientation(orientation2);
+        assertEquals(orientation2, mo.getOrientation().getValue(), delta);
+    }
+
 }
