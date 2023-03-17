f0b953a9b5c7348b20fb61fd376f0aa09ca4dd26
jum002 <jum002@ucsd.edu>
2023-03-16 22:48:17 -0700

    Implemented US 14 and major refactoring in CompassActivity

--- app/src/main/java/com/example/socialcompass/Activities/CompassActivity.java
+++ app/src/main/java/com/example/socialcompass/Activities/CompassActivity.java
@@ -1,26 +1,17 @@
 package com.example.socialcompass.Activities;
 
-import static androidx.constraintlayout.helper.widget.MotionEffect.TAG;
-
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.constraintlayout.widget.ConstraintLayout;
-import androidx.constraintlayout.widget.ConstraintSet;
+import androidx.core.util.Pair;
 import androidx.lifecycle.LiveData;
 import androidx.lifecycle.ViewModelProvider;
 
 import android.content.SharedPreferences;
-import android.graphics.Color;
 import android.os.Bundle;
-import android.text.TextUtils;
-import android.util.Log;
 import android.view.View;
-import android.view.ViewGroup;
-import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
 
-import com.example.socialcompass.Compass;
-import com.example.socialcompass.Display;
 import com.example.socialcompass.Models.Person;
 import com.example.socialcompass.Orientation;
 import com.example.socialcompass.Services.LocationService;
@@ -29,42 +20,42 @@ import com.example.socialcompass.Point;
 import com.example.socialcompass.R;
 import com.example.socialcompass.Utils.CompassBg;
 import com.example.socialcompass.Utils.CompassMarkers;
+import com.example.socialcompass.Utils.GPSSignalStatus;
 import com.example.socialcompass.ViewModels.FriendListViewModel;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Map;
 
 public class CompassActivity extends AppCompatActivity {
 
-    SharedPreferences preferences;
-    FriendListViewModel viewModel;
+    private final int REFRESH_INTERVAL_MILLIS = 5000;
 
+    private SharedPreferences preferences;
+    private FriendListViewModel viewModel;
+
+    private int numPeople;  // user + number of friends
+    private Person user;
+    private LiveData<List<Person>> friends;
+    private Point currentLocation;
+    private List<Point> friendLocations;
+
+    private long prevTime = 0;
     private int zoomLevel = 2; // technically number of circles
 
-
     private LocationService locationService;
-
-    private LiveData<List<Person>> people;
-
-    private long prevTime;
-    private Person user;
-
     private OrientationService orientationService;
-    public static Float device_orientation =0f;
+    public static Float device_orientation = 0f;
     private Orientation orientation;
-
-
-    Point currentLocation;
-    List<Point> friendLocations;
-
     Orientation mockorientation;
 
+    private ConstraintLayout layout;
     private TextView orientationText;
-
+    private TextView timer;
+    private ImageView statusIcon;
 
     private CompassBg compassBg;
     private CompassMarkers compassMarkers;
+    private GPSSignalStatus signalStatus;
 
 
     @Override
@@ -74,69 +65,18 @@ public class CompassActivity extends AppCompatActivity {
 
         wireWidgets();
 
-        locationService = locationService.singleton(this);
-        orientationService = orientationService.singleton(this);
+        initializeServices();
+        initializeStorage();
+        initializeUser();
+        initializeFriends();
+        initializeLocations();
+        initializeOrientation();
+        initializeViewComponenets();
 
-
-        preferences = getSharedPreferences("main", MODE_PRIVATE);
-        int numPeople = preferences.getAll().size() - 1;
-        if(numPeople<1){
-            numPeople = 1;
-        }
-        String userUID = preferences.getString("0", "default_if_not_found");
-        String[] friendsUID = new String[numPeople - 1];
-
-        for (int i = 1; i < numPeople; i++) {
-            friendsUID[i - 1] = preferences.getString("" + i, "default_if_not_found");
-        }
-
-        viewModel = setupViewModel();
-        people = viewModel.getRemoteFriends(friendsUID);
-        people.observe(this, locList->{
-            for (int i = 0; i < locList.size(); i++) {
-                Log.d("Compass Test", locList.get(i).toString());
-                friendLocations.get(i).setLabel(locList.get(i).name);
-                friendLocations.get(i).setLocation(locList.get(i).latitude, locList.get(i).longitude);
-            }
-        });
-
-        Log.d("Compass Test", userUID);
-
-        String userName = preferences.getString("name", "");
-        user = new Person(userName, userUID, 0,0);
-
-        getOrientation();
-
-        currentLocation = new Point();
-        friendLocations = new ArrayList<>();
-
-        for (int i = 1; i < numPeople; i++) {
-            friendLocations.add(new Point());
-        }
-
-        ConstraintLayout layout = findViewById(R.id.Compass);
-        compassBg = new CompassBg(layout, this, zoomLevel);
-        compassMarkers = new CompassMarkers(friendLocations, currentLocation, orientation, layout, this);
-
-        prevTime = 0;
-        locationService.getLocation().observe(this, loc->{
-            if (System.currentTimeMillis() - prevTime > 5000) {
-                currentLocation.setLocation(loc.first.floatValue(), loc.second.floatValue());
-                compassMarkers.setCurrentLocation(currentLocation);
-                user.latitude = loc.first.floatValue();
-                user.longitude = loc.second.floatValue();
-                viewModel.updateUser(user);
-                prevTime = System.currentTimeMillis();
-            }
-        });
-        orientationService.getOrientation().observe(this, orient ->{
-            device_orientation = (Float)orient;
-            orientation.setOrientationFromRadians(device_orientation);
-            orientation.setOrientation(orientation.getOrientationInDegrees() + mockorientation.getOrientationInDegrees());
-            compassMarkers.setCurrentOrientation(orientation);
-            // Log.d("testing", "" + orientation.getOrientationInDegrees());
-            compassMarkers.update(zoomLevel);
-        });
+        // Observers
+        friends.observe(this, this::updateFriendLocations);
+        locationService.getLocation().observe(this, this::updateLocations);
+        orientationService.getOrientation().observe(this, this::updateOrientations);
     }
 
     @Override
@@ -146,11 +86,10 @@ public class CompassActivity extends AppCompatActivity {
     }
 
     void wireWidgets() {
+        layout = findViewById(R.id.Compass);
         orientationText = findViewById(R.id.editOrientation);
-    }
-
-    private FriendListViewModel setupViewModel() {
-        return new ViewModelProvider(this).get(FriendListViewModel.class);
+        timer = findViewById(R.id.DC_timer);
+        statusIcon = findViewById(R.id.connection_status);
     }
 
     public void OkbtnClicked(View view) {
@@ -169,7 +108,23 @@ public class CompassActivity extends AppCompatActivity {
         compassBg.update(zoomLevel);
     }
 
-    void getOrientation(){
+
+
+    private void initializeStorage() {
+        preferences = getSharedPreferences("main", MODE_PRIVATE);
+        numPeople = Math.max(1, preferences.getAll().size() - 1);
+    }
+
+    private void initializeServices() {
+        locationService = locationService.singleton(this);
+        orientationService = orientationService.singleton(this);
+    }
+
+    private FriendListViewModel setupViewModel() {
+        return new ViewModelProvider(this).get(FriendListViewModel.class);
+    }
+
+    void initializeOrientation(){
         Bundle extras = getIntent().getExtras();
 
         if (extras != null) {
@@ -177,8 +132,72 @@ public class CompassActivity extends AppCompatActivity {
         } else {
             mockorientation = new Orientation();
         }
-
         orientation = new Orientation(device_orientation);
-
     }
+
+    void initializeLocations() {
+        currentLocation = new Point();
+        friendLocations = new ArrayList<>();
+
+        for (int i = 1; i < numPeople; i++) {
+            friendLocations.add(new Point());
+        }
+    }
+
+    void initializeUser() {
+        String userName = preferences.getString("name", "no name");
+        String userUID = preferences.getString("0", "no UID");
+        user = new Person(userName, userUID, 0,0);
+    }
+
+    private void initializeViewComponenets() {
+        compassBg = new CompassBg(layout, this, zoomLevel);
+        compassMarkers = new CompassMarkers(friendLocations, currentLocation, orientation, layout, this);
+        signalStatus = new GPSSignalStatus(timer, statusIcon);
+    }
+
+    private void initializeFriends() {
+        String[] friendsUID = new String[numPeople - 1];
+
+        for (int i = 1; i < numPeople; i++) {
+            friendsUID[i - 1] = preferences.getString("" + i, "no friend");
+        }
+
+        viewModel = setupViewModel();
+        friends = viewModel.getRemoteFriends(friendsUID);
+    }
+
+
+
+    void updateLocations(Pair<Double, Double> loc) {
+        signalStatus.updateTime(System.currentTimeMillis());
+
+        if (System.currentTimeMillis() - prevTime > REFRESH_INTERVAL_MILLIS) {
+            currentLocation.setLocation(loc.first.floatValue(), loc.second.floatValue());
+            compassMarkers.setCurrentLocation(currentLocation);
+            user.latitude = loc.first.floatValue();
+            user.longitude = loc.second.floatValue();
+            viewModel.updateUser(user);
+            prevTime = System.currentTimeMillis();
+        }
+    }
+
+    private void updateOrientations(Float orient) {
+        device_orientation = (Float)orient;
+        orientation.setOrientationFromRadians(device_orientation);
+        orientation.setOrientation(orientation.getOrientationInDegrees() + mockorientation.getOrientationInDegrees());
+        compassMarkers.setCurrentOrientation(orientation);
+        // Log.d("testing", "" + orientation.getOrientationInDegrees());
+        compassMarkers.update(zoomLevel);
+        signalStatus.update();
+    }
+
+    private void updateFriendLocations(List<Person> locList) {
+        for (int i = 0; i < locList.size(); i++) {
+            // Log.d("Compass Test", locList.get(i).toString());
+            friendLocations.get(i).setLabel(locList.get(i).name);
+            friendLocations.get(i).setLocation(locList.get(i).latitude, locList.get(i).longitude);
+        }
+    }
+
 }
